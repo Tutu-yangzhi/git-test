7f39a37c97e2dc88a718b7f492b8555b2d47fce3
yangzhi <yangzhi@git.com>
2023-07-08 15:00:16 +0800

    second commit

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -9,5 +9,6 @@ package com;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("second commit");
     }
 }
