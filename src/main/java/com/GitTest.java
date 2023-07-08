ee8438241d46d5147b68bebbb94dd6041f59070f
yangzhi <yangzhi@git.com>
2023-07-08 14:58:54 +0800

    first commit

---
+++ src/main/java/com/GitTest.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @Auther: YangZhi
+ * @Date:Created in 14:54 2023/7/8
+ * @Description:
+ * @Modified By:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("first commit");
+    }
+}
